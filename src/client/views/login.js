f4184a0c41f0ffa15a9a01dcca5a2f0b4c92f7f0
unknown <belladonnacrystal@gmail.com>
2020-07-01 15:01:31 +1200

    more test

--- src/client/views/login.js
+++ src/client/views/login.js
@@ -2,9 +2,5 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
 export const Login = () => (
-    <Route {...rest} render={props => (
         localStorage.setItem('Authorized', true)
-            ? <Component {...props} />
-            : <Redirect to={{ pathname: '/', state: { from: props.location } }} />
-    )} />
 )
